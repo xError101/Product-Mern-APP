d0860c231313c5d8f24dbc5ec75d917121dfba23
xError101 <error.not.known.404@gmail.com>
2024-09-18 14:21:47 +0100

    NavBar added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Box } from "@chakra-ui/react";
+import {Route,Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
+import {useColorModeValue } from '@chakra-ui/react';
+
+
+function App() {
+  return (
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/create" element={<CreatePage />}/>
+      </Routes>
+    </Box>
+  )
+}
+
+export default App
